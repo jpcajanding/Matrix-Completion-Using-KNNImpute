faf198608b7402696cd4b654b638ab259b9dde84
jpcajanding <jpmcajanding@gmail.com>
2017-12-07 16:12:37 +0800

    Finished Masking
    finished time based masking
    Updated low_rank_matrix for time based masking and norm calculation

--- low_rank_matrix.py
+++ low_rank_matrix.py
@@ -4,19 +4,22 @@ from numpy import ma as ma
 import pandas as pd
 import knnimpute
 
-from masking import random_masking
+from masking import random_masking, time_masking
 
 datafile = 'data\winequality-red.csv'
 data = pd.read_csv(datafile,delimiter=';')
 
-print(data['fixed acidity'][10])
-x = np.arange(15,1599)
-data1 = data #.drop(x)
-x = np.transpose(data1)
-row, col = x.shape
+# print(data['fixed acidity'][10])
+print(data.shape)
+# data1 = data #.drop(x)
+masked_data, mask = time_masking(data,mask_time=0.01)
+masked_data = np.transpose(masked_data)
+mask = np.transpose(mask)
 
 #mask random positions in a matrix
-masked_data, mask = random_masking(x)
+# masked_data, mask = random_masking(x)
 
-filled_data = knnimpute.knn_impute_few_observed(masked_data,mask,5)
+# filled_data = knnimpute.knn_impute_few_observed(masked_data,mask,5)
+filled_data = knnimpute.knn_impute_with_argpartition(masked_data,mask,5)
 
+print(np.linalg.norm((data-(np.transpose(filled_data)))))
--- masking.py
+++ masking.py
@@ -11,7 +11,27 @@ def random_masking(data, num_mask=10):
     mask = masked_data.mask
 
     masked_data = masked_data.filled(np.NaN)
+
     return masked_data, mask
 
-def time_masking(data, masked_row = 1, time = 0.1, resolution = 0.004):
+def time_masking(data, masked_node = 2, mask_time = 0.1, resolution = 0.004, start_percentage = 0.10):
     row, col = data.shape
+
+    #the matrix is an 9xn matrix where n is the total number of data.
+    #need to mask a block of 3xm where m is dependent on the resolution
+    start_index = int(row * start_percentage)
+    num_mask = int(mask_time/resolution)
+    if start_index + num_mask < row:
+        mask = np.zeros((row,col))
+
+        #columns 0-2 for node 1, 3-5 for node 2 and cols 6-8 for node 3
+        mask_cols = np.array([3*(masked_node-1),3*(masked_node-1)+1,3*(masked_node-1)+2])
+        masking = np.ones((num_mask,3))
+        print(masking.shape)
+        mask[start_index:(start_index+num_mask),(3*(masked_node-1)):(3*(masked_node-1)+3)] = masking
+        masked_data = ma.masked_array(data, mask)
+        mask = masked_data.mask
+
+        masked_data = masked_data.filled(np.NaN)
+
+    return masked_data, mask
